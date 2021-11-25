5d911a27ead5de3a995952a152e48e3c01c6d973
Nitjareent <35363752+Nitjareent@users.noreply.github.com>
2021-11-25 21:57:25 +0700

    Update Function1.js

---
+++ src/pages/Function1.js
@@ -0,0 +1,39 @@
+import { useState } from "react" 
+export default function Function1(){
+    const [input, setInput] = useState('')
+    const [array, setArray] = useState([])
+    const [result, setResult] = useState('')
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+    const handleClick = () => {
+        
+        const newArray = input.split(',')
+        const mapArray = newArray.map((val)=>{return parseInt(val, 10)})
+        if (mapArray.length>1){
+            const max = Math.max.apply(null, mapArray)
+            const maxNum = mapArray.filter(element => element===max)
+            console.log(maxNum.length);
+            mapArray.splice(mapArray.indexOf(max), maxNum.length)
+            setResult(Math.max.apply(null, mapArray))
+        }
+        else if (mapArray.length===0||isNaN(mapArray)){
+            setResult('null')
+        }else if (mapArray.length===1){
+            setResult(mapArray)
+        }
+       
+    }
+    
+    return (
+        <div>
+            <label >Enter your array:</label>
+            <input onChange={(e) => handleChange(e)} type="text"  name="yourArray" />
+            <button onClick={() => handleClick()}>
+                Enter
+            </button>
+            <p>{result}</p>
+        </div>
+        
+    )
+}
